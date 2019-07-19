159a6973c3e09b486059f0c4e167328aeef353ea
Charisse Strong <msstrong92@gmail.com>
2019-07-18 22:04:14 -0400

    app

--- condition/src/App.js
+++ condition/src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <nav>
+          <ul>
+            <li><button>Welcome</button></li>
+            <li><button>All Planets</button></li>
+            <li><button>See a Random Planet</button></li>
+          </ul>
+        </nav>
+        <h1>Majestic's Planetarium</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
